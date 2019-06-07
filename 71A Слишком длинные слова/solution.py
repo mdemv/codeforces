bf76b08d9086eec7b116dc9bc9d5185aa2f98e76
mdemichev <mdemichev@technoserv.com>
2019-06-07 15:54:28 +0300

    +

---
+++ 71A Слишком длинные слова/solution.py
@@ -0,0 +1,12 @@
+linestotal = input()
+
+r = []
+for line in range(int(linestotal)):
+    w = input()
+    if len(w) > 10:
+      r.append(w[0] + str(len(w) - 2) + w[-1])
+    else:
+      r.append(w)
+
+for word in r:
+  print(word)
